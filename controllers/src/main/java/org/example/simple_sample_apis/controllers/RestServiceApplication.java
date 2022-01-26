190d9b8d4aa30db210c582b9cf90d389c261ac03
Anton de Craen <anton.decraen@unifiedpost.com>
2022-01-26 17:48:02 +0100

    add packages to component-scan

--- controllers/src/main/java/org/example/simple_sample_apis/controllers/RestServiceApplication.java
+++ controllers/src/main/java/org/example/simple_sample_apis/controllers/RestServiceApplication.java
@@ -3,7 +3,7 @@ package org.example.simple_sample_apis.controllers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.example.simple_sample_apis.controllers", "org.example.simple_sample_apis.usecase_factories"})
 public class RestServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(RestServiceApplication.class, args);
