8da550d07af542ebdab78fe1fcc7c7e6c3ff0082
Anton de Craen <anton.decraen@unifiedpost.com>
2022-01-28 11:19:39 +0100

    rename application to SimpleSampleApisApplication

--- app/src/main/java/org/example/simple_sample_apis/app/SimpleSampleApisApplication.java
+++ app/src/main/java/org/example/simple_sample_apis/app/SimpleSampleApisApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = { "org.example.simple_sample_apis.app", "org.example.simple_sample_apis.app.usecase_factories", "org.example.simple_sample_apis.http_client",  })
-public class RestServiceApplication {
+public class SimpleSampleApisApplication {
   public static void main(String[] args) {
-    SpringApplication.run(RestServiceApplication.class, args);
+    SpringApplication.run(SimpleSampleApisApplication.class, args);
   }
 }
