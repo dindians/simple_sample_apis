c0c45bb6d4d22dd9bcba643bd44fc3fa2819f68f
Anton de Craen <anton.decraen@unifiedpost.com>
2022-02-13 19:48:35 +0100

    delete HasHttpStatus from app module

--- app/src/main/java/org/example/simple_sample_apis/app/HasHttpStatusCode.java
+++
@@ -1,5 +0,0 @@
-package org.example.simple_sample_apis.app;
-
-public interface HasHttpStatusCode {
-  Integer getHttpStatusCode();
-}
--- app/src/main/java/org/example/simple_sample_apis/app/HttpResponseNotOk.java
+++ app/src/main/java/org/example/simple_sample_apis/app/HttpResponseNotOk.java
@@ -2,6 +2,7 @@ package org.example.simple_sample_apis.app;
 
 import org.jetbrains.annotations.NotNull;
 import org.example.simple_sample_apis.fp.EitherError;
+import org.example.simple_sample_apis.rest_controllers.HasHttpStatusCode;
 
 public final class HttpResponseNotOk implements EitherError, HasHttpStatusCode {
   private final String uri;
